12cf8a90e4b70cb654476181e0ea24a666234bd8
riku <swallowtail.a7@gmail.com>
2019-12-09 03:11:00 +0900

    Add sample client modules

---
+++ src/main/resources/client-app/src/views/Login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="login-page">
+    <h1>Login Page</h1>
+    <div class="input-area">
+      <div>ユーザ名</div>
+      <input type="text" v-model="loginId" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component({
+  name: 'login',
+  data() {
+    return {
+      userName: '',
+      password: '',
+    };
+  },
+})
+export default class Login extends Vue {
+  private userName: string;
+  private password: string;
+
+  private login(): void {
+    const req = {
+      userName: this.userName,
+      password: this.password,
+    };
+  }
+}
+</script>
